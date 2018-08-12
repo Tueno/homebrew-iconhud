2804681073bf56f41f1932c82c092a9311b9ea3e
Tueno <d3l4ynk+github@gmail.com>
2018-08-12 14:53:55 +0900

    Update IconHUD version

--- iconhud.rb
+++ iconhud.rb
@@ -1,8 +1,8 @@
 class Iconhud < Formula
   desc ""
   homepage "https://github.com/Tueno/IconHUD"
-  url "https://github.com/Tueno/IconHUD/archive/1.0.zip"
-  sha256 "82475de0ff11ec2de2e90862a63790d7933a27c176ee4011ba25451db0ca5956"
+  url "https://github.com/Tueno/IconHUD/archive/1.1.zip"
+  sha256 "a7ec9f0578996fe0db93423028da97d631a1824153b58be34a40ad7fb917c1b2"
 
   depends_on :xcode => ["8.0", :build]
   depends_on "ghostscript"
